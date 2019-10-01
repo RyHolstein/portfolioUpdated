1784c4259aa46ebda1a953940a18cc6f446ed513
Ryan Holstein <ryan.holstein@caravive.com>
2019-10-01 13:11:17 -0700

    added caution

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -11,26 +11,29 @@ const Home = props => {
             <div className='logo'>
 
                 <div className='links'>
-                    <a className='links__link' href='mailto:me@ryanholste.in'>
+                    <a className='links__link first' href='mailto:me@ryanholste.in'>
                         <i className="fas fa-envelope"></i>
                     </a>
-                    <a className='links__link' href='https://www.linkedin.com/in/ryanholstein25/' target="_blank">
+                    <a className='links__link second' href='https://www.linkedin.com/in/ryanholstein25/' target="_blank">
                         <i className="fab fa-linkedin-in"></i>
                     </a>
-
-                    <a className='links__link' href='https://github.com/Terrymillz' target="_blank">
-                        <i className="fab fa-github"></i>
-                    </a>
-                    <a className='links__link' href='https://twitter.com/Sexy_RyRy' target="_blank">
+                    <a className='links__link third' href='https://twitter.com/Sexy_RyRy' target="_blank">
                         <i className="fab fa-twitter"></i>
                     </a>
+                    <a className='links__link fourth' href='https://github.com/Terrymillz' target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                   
                 </div>
 
                 <div className='logo__grad-bar'></div>
                 <p className='logo__name'>Ryan Holstein</p>
                 <p className='logo__subtitle'>Web Developer</p>
             </div>
-
+            <div className='caution-sign'>
+                <p className='top-row'>caution!</p>
+                <p className='bottom-row'>This site is currently under maintenance</p>
+            </div>
            
         </div>
 
